4376f8650a836275cee0ce93082fe15dd0470958
rster2002 <rster2002dev@gmail.com>
2022-12-06 10:26:17 +0100

    ✨ Finish day 6 part 2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -8,19 +8,22 @@ fn main() {
         .expect("Please provide an input for the program");
 
     let file_content = fs::read_to_string(file_path).expect("Could not read file");
+    const BUFFER_SIZE: usize = 14;
 
     // Still use .lines to get rid of any \n
     let input = file_content.lines().next().unwrap();
-    let mut buffer: BoundedVecDeque<char> = BoundedVecDeque::new(4);
+    let mut buffer: BoundedVecDeque<char> = BoundedVecDeque::new(BUFFER_SIZE);
 
     for (i, char) in input.chars().enumerate() {
         buffer.push_back(char);
 
-        if buffer.len() == 4 && all_unique(&buffer) {
+        if buffer.len() == BUFFER_SIZE && all_unique(&buffer) {
             println!("Char: {}", i + 1);
-            break;
+            return;
         }
     }
+
+    println!("No marker found");
 }
 
 fn all_unique(buffer: &BoundedVecDeque<char>) -> bool {
